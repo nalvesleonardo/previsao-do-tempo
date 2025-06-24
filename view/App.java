afbdea99b05f69435464b6a8f3dee53ddb5ad17a
nalvesleonardo <leonardoalves6558@gmail.com>
2025-06-24 20:42:11 -0300

    Rename main.java to App.java and update package declaration; modify misc.xml to specify project JDK version

---
+++ view/App.java
@@ -0,0 +1,8 @@
+package view;
+
+public class App {
+    public static void main(String[] args) {
+        FrmPrevisaoTempo frm = new FrmPrevisaoTempo(); // Cria e exibe a janela
+        frm.setVisible(true);
+    }
+}
--- view/main.java
+++
@@ -1,9 +0,0 @@
-import view.FrmPrevisaoTempo;
-import view.WeatherDashboardApp;
-
-import javax.swing.*;
-
-public static void main(String[] args) {
-    FrmPrevisaoTempo frm = new FrmPrevisaoTempo(); // Cria e exibe a janela
-    frm.setVisible(true);
-}
